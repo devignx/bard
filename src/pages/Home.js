87efd1d8bc9d423a7b4bd891fecaa119c2fe3b3f
Devign Experience <devignx@gmail.com>
2023-02-07 23:12:10 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
 
     return(
         <div className=" relative">
-            <div className='fixed top-10 right-5 '>
-                <a href='/app' className='toppp bg-black/90 transition-all duration-300 ease-in-out hover:bg-black text-white px-8 py-4 rounded-full'>Open App</a>
+            <div className='toppp fixed top-10 right-5 '>
+                <a href='/app' className='md:bg-black/90 transition-all duration-300 ease-in-out hover:bg-black text-white px-8 py-4 rounded-full'>Open App</a>
             </div>
             <section className='h-screen flex flex-col justify-center items-center'>
                     <img className='w-[10rem]' src={brd} />
@@ -29,7 +29,7 @@ const Home = () => {
             </section>
 
             <section id='about' className='relative'>
-                <div>
+                <div className='mt-12'>
                     <h2 className='font-black text-center text-4xl rale'>About BARD-AI</h2>
                     <p className='w-10/12 md:w-[60%] mt-12 m-auto text-justify md:text-center'>
                         Technology giants, Microsoft and Google, go head to head in the Artificial Intelligence race as Google anounces the release of BardAI for public access as a response to ChatGBT, the other AI program that is currently open to the public.
@@ -47,10 +47,10 @@ const Home = () => {
 
             <section>
                 <div className='flex gap-12 p-12 justify-center mt-12'>
-                    <a className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://t.me/BardAi_ERC'><img src={tel} /></a>
-                    <a className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://www.dextools.io/app/en/ether/pair-explorer/0x948b0bab68c1fbd6af824a0ad56513c8dd3bb778'><img src={dex} /></a>
-                    <a className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://twitter.com/bardai_erc20'><img src={twt} /></a>
-                    <a className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://etherscan.io/address/0x3b88BB02BD2F50EEdf30DE6B94Cd5351CecD3658'><img src={eth} /></a>
+                    <a target='_blank' className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://t.me/BardAi_ERC'><img src={tel} /></a>
+                    <a target='_blank' className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://www.dextools.io/app/en/ether/pair-explorer/0x948b0bab68c1fbd6af824a0ad56513c8dd3bb778'><img src={dex} /></a>
+                    <a target='_blank' className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://twitter.com/bardai_erc20'><img src={twt} /></a>
+                    <a target='_blank' className='opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out' href='https://etherscan.io/address/0x3b88BB02BD2F50EEdf30DE6B94Cd5351CecD3658'><img src={eth} /></a>
                 </div>
                 <br/>
                 <p className='my-12 text-center text-xs'>Copyrights recieved by BARD AI - 2023</p>
